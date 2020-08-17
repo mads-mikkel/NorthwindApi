ed1210449a81a9852117588eac7d69a0ffa387a0
Mads Mikkel Rasmussen <mara@aspit.dk>
2020-08-17 11:58:41 +0200

    Add project files.

---
+++ NorthwindApi.DataAccess/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace NorthwindApi.DataAccess
+{
+    public class Class1
+    {
+    }
+}
---
+++ NorthwindApi.Entities/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace NorthwindApi.Entities
+{
+    public class Class1
+    {
+    }
+}
